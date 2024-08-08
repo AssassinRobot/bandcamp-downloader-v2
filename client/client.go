c2f350ea3218fc1ba7cc6a2800802cf2896010e5
Amir <assassinmoltafet@gmail.com>
2024-08-09 00:21:32 +0330

    include client

---
+++ client/client.go
@@ -0,0 +1,201 @@
+package client
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/crsrusl/bandcamp-downloader-v2/entities"
+	"github.com/crsrusl/bandcamp-downloader-v2/file"
+	"github.com/crsrusl/bandcamp-downloader-v2/helpers"
+)
+
+type Client interface {
+	Download(url string)[]error
+	loadPage(url string) error
+	downloadImage(filepath, url string) error
+	downloadMP3(mp3 *entities.TrackData) error
+}
+
+func NewClient(file file.File)Client{
+	return &bandcampClient{
+		client:  &http.Client{},
+		wg: &sync.WaitGroup{},
+		downloads: &[]string{},
+		file: file,
+	}
+}
+
+type bandcampClient struct {
+	client    *http.Client
+	file      file.File
+	downloads *[]string
+	doc       *goquery.Document
+	wg        *sync.WaitGroup
+}
+
+func (c *bandcampClient) Download(url string) []error{
+	var errors []error
+	
+	var errorChan = make(chan error, 500)
+
+	loadPageError := c.loadPage(url)
+	if loadPageError != nil {
+		errors = append(errors, loadPageError)
+	}
+
+	c.doc.Find("script[data-tralbum]").Each(func(i int, s *goquery.Selection) {
+		var trackData = &entities.TrackData{}
+
+		ticker := helpers.DownloadStatus(c.downloads)
+
+		trackDataString, _ := s.Attr("data-tralbum")
+
+		if jsonUnmarshalError := json.Unmarshal([]byte(trackDataString), &trackData); jsonUnmarshalError != nil {
+			log.Fatal(jsonUnmarshalError)
+		}
+
+		baseFilepath := fmt.Sprintf("./%s%s", helpers.RemoveAlphaNum(trackData.Artist), helpers.RemoveAlphaNum(trackData.Current.Title))
+		albumArtwork := fmt.Sprintf("https://f4.bcbits.com/img/a%s_16.jpg", strconv.Itoa(trackData.Current.ArtID))
+		albumArtworkFilepath := fmt.Sprintf("%s/%s.jpg", baseFilepath, trackData.Current.Title)
+
+		createError := c.file.CreateDir(baseFilepath)
+		if createError != nil {
+			errors = append(errors, createError)
+		}
+
+		downloadImageError := c.downloadImage(albumArtworkFilepath, albumArtwork)
+		if downloadImageError != nil {
+			errors = append(errors, downloadImageError)
+		}
+
+		for _, v := range trackData.TrackInfo {
+			c.wg.Add(1)
+			trackData.CurrentTrackNum = strconv.Itoa(v.TrackNum)
+			trackData.CurrentTrackTitle = v.Title
+			trackData.CurrentTrackURL = v.File.Mp3128
+			trackData.CurrentTrackFilepath = baseFilepath +
+				"/" + helpers.RemoveAlphaNum(trackData.CurrentTrackNum) +
+				"-" + helpers.RemoveAlphaNum(trackData.Artist) +
+				"-" + helpers.RemoveAlphaNum(trackData.CurrentTrackTitle) +
+				".mp3"
+			go func() {
+				downloadError := c.downloadMP3(trackData)
+				if downloadError != nil{
+					errorChan <- downloadError
+				}
+			}()
+		}
+
+		c.wg.Wait()
+
+		ticker.Stop()
+
+		close(errorChan)
+
+	})
+
+	for err := range errorChan{
+		errors = append(errors, err)
+	}
+
+	return errors
+}
+
+func (c *bandcampClient) loadPage(url string) error {
+	log.Println("Getting... ", url)
+
+	req, httpRequestError := http.NewRequestWithContext(context.TODO(), "GET", url, nil)
+	if httpRequestError != nil {
+		return httpRequestError
+	}
+
+	res, httpGetError := c.client.Do(req)
+	if httpGetError != nil {
+		return httpGetError
+	}
+
+	defer func() {
+		err := res.Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	doc, documentReaderError := goquery.NewDocumentFromReader(res.Body)
+
+	if documentReaderError != nil {
+		return documentReaderError
+	}
+
+	c.doc = doc
+
+	return nil
+}
+
+func (c *bandcampClient) downloadMP3(mp3 *entities.TrackData) error {
+	defer c.wg.Done()
+
+	*c.downloads = append(*c.downloads, fmt.Sprintf("%s - %s", mp3.Artist, mp3.CurrentTrackTitle))
+
+	req, httpRequestError := http.NewRequestWithContext(context.TODO(), "GET", mp3.CurrentTrackURL, nil)
+
+	if httpRequestError != nil {
+		return httpRequestError
+	}
+
+	resp, httpGetError := c.client.Do(req)
+	if httpGetError != nil {
+		return httpGetError
+	}
+
+	defer func() {
+		err := resp.Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	saveError := c.file.Save(mp3.CurrentTrackFilepath, resp.Body)
+	if saveError != nil {
+		return saveError
+	}
+
+	tagFileError := c.file.TagFile(mp3)
+
+	if tagFileError != nil {
+		return tagFileError
+	}
+
+	return nil
+}
+
+func (c *bandcampClient) downloadImage(filepath string, url string) error {
+	resp, httpGetError := http.Get(url)
+	if httpGetError != nil {
+		return httpGetError
+	}
+
+	defer func() {
+		err := resp.Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	saveError := c.file.Save(filepath, resp.Body)
+	if saveError != nil {
+		return saveError
+	}
+
+	return nil
+}
